ec49baec258c53c6cd138a4ea29390303e4d8c47
Om <om@outlook.com>
2019-06-10 18:13:59 +0800

    ruby class week_6

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,81 @@
 class Bootcamp
-  
+
+    def initialize (name,slogan,student_capacity)
+        @name = name 
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers=[]
+        @students=[]
+        @grades=Hash.new { |hash, key| hash[key] = [] }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers.push(teacher)
+    end
+
+    def enroll(student)
+        if @students.size<6
+            @students.push(*student)
+            return true
+        else
+            @student_capacity
+            false
+        end
+    end
+
+    def enrolled?(student_name)
+        return @students.include?student_name
+    end
+
+    def student_to_teacher_ratio
+        (@students.size/@teachers.size) #.ceil
+    end
+
+    def add_grade(student,grade)
+        if @students.include?student
+            @grades[student].push(grade)
+            true
+        else
+            false
+        end
+    end
+
+    def num_grades(student)
+        @grades[student].size
+    end
+
+
+    def average_grade(student)
+        if enrolled?(student)
+            if @grades[student].size ==0
+                return nil
+            else
+                total = @grades[student].inject{|acc,el|acc+el}
+                return total/(@grades[student].size)
+            end
+        else
+            return nil
+        end
+    end
+                
+
+
+
 end
--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,59 @@
+
+
 class Dog
+
+    def initialize (name,breed,age,bark,favorite_foods)
+        @name = name 
+        @breed = breed 
+        @age = age 
+        @bark = bark
+        @favorite_foods = favorite_foods
+
+    end
+
+    def name
+        @name
+    end
+
+    def breed 
+        @breed 
+    end
+
+    def age 
+        @age
+    end
+
+    def age=age
+        @age=age
+    end
+
+
+    def bark
+        if @age>3
+            @bark.upcase
+        else
+            @bark.downcase
+        end
+    end
+
+
+    def favorite_foods
+        @favorite_foods
+       
+    end
+
+    def favorite_food?(food_item)
+        # return food_item
+        for item in @favorite_foods
+            if food_item.downcase == item.downcase
+                return true
+           
+            end
+        
+        end
+        false
+    end
+
+
 
 end
